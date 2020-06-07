0493688ad12bbf5a39a1f678ecfbdfe82e584af4
Christopher Cole <ccole@well.ox.ac.uk>
2020-06-06 20:52:38 -0400

    Dont bother about the order

--- tests/main.rs
+++ tests/main.rs
@@ -19,8 +19,9 @@ fn test_success_run() {
     let output = out
         .get_output();
     let known_out = "barcode1 0.33333\nbarcode2 0.14286\n";
+    let known_out2 = "barcode2 0.14286\nbarcode1 0.33333\n";
 
     println!("{:?}", output);
     println!("{:?}", out);
-    assert!(output.stdout == known_out.as_bytes().to_vec());
+    assert!(output.stdout == known_out.as_bytes().to_vec() || output.stdout == known_out2.as_bytes().to_vec() );
 }
